f14da5119452a23f478856c99b249c2bb1297df5
Ville Svärd <ville.svard@agical.com>
2011-11-27 22:15:19 +0100

    Upgraded to lastest clojure and midje

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject yahtzee "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [midje "1.1.1"]]
-  :dev-dependencies [[swank-clojure "1.3.1"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [midje "1.3-alpha5"]]
+  :dev-dependencies [[swank-clojure "1.3.1"]
+                     [lein-midje "1.0.5"]])
