145d2b572b38ea18b3061ef8c1822a611f4ec8ca
Ville Svärd <ville.svard@agical.com>
2013-10-16 20:56:26 +0200

    Removed swank clojure

--- project.clj
+++ project.clj
@@ -2,5 +2,4 @@
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [midje "1.3-alpha5"]]
-  :dev-dependencies [[swank-clojure "1.3.1"]
-                     [lein-midje "1.0.5"]])
+  :dev-dependencies [[lein-midje "1.0.5"]])
